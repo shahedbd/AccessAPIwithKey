42c4bfd7f8dc995bd6781ef1b83aa8e272f5f86f
R M Shahidul Islam <shahedbddev@gmail.com>
2024-03-06 07:53:50 +0600

    + Project code added

---
+++ WebAPI.GenericCRUD_D8/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace WebAPI.GenericCRUD_D8.Middlewares
+{
+    public class ApiKeyMiddleware
+    {
+        private const string API_KEY_HEADER_NAME = "X-Api-Key";
+        private readonly RequestDelegate _next;
+        private readonly string _validApiKey;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _validApiKey = configuration["TenantInfo:APIKey"];
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(API_KEY_HEADER_NAME, out var receivedApiKey) || receivedApiKey != _validApiKey)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Invalid or missing API Key.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
